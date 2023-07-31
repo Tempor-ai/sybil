0e9dc951c42483e0c6e3899ec1444e2729797dc7
sirbradflies <fbaldisserri@gmail.com>
2023-07-31 11:29:17 -0500

    Aligned to Nixtla interface and returning trained model

--- m2_prototype.py
+++ m2_prototype.py
@@ -1,51 +1,105 @@
+"""
+Prototype for the 2nd milestone of the project.
+Using the Statsforecast wrapper to train a base model and predict on a dataset.
+"""
+import argparse
+import numpy as np
 import pandas as pd
-from statsforecast.models import AutoTheta
-from modelwrappers import StatsforecastWrapper
+from statsforecast.models import AutoTheta, AutoETS
+from modelwrappers import AbstractModel, StatsforecastWrapper
 from sklearn.model_selection import train_test_split
 from ts_utils import calculate_smape, get_seasonal_period
 
-#DATASET_PATH = "datasets/SF_hospital_load.csv"
-DATASET_PATH = "datasets/retail/air_passengers.csv"
+DATASET_PASSENGER = "datasets/retail/air_passengers.csv"
 TIME_COL = 'ds'
 VALUE_COL = 'y'
 
 
 def main():
-    # Load dataset
-    dataset = pd.read_csv(DATASET_PATH)
+    # Read the dataset path from the command line
+    parser = argparse.ArgumentParser(description='Train a model using Statsforecast and predict on a dataset.')
+    parser.add_argument('-d', '--dataset', type=str,
+                        help='Path or url to the dataset csv file.',
+                        default=DATASET_PASSENGER)
+    args = vars(parser.parse_args())
 
+    # Load dataset
+    dataset = pd.read_csv(args['dataset'])
+
+    # Train model
+    training_info = train(dataset)
+    print(f'Training Complete: {training_info}')
+
+    model = training_info['model']
+    season_length = training_info['stats']['season_length']
+
+    # Predict next steps
+    y_pred = predict(model=model, lookforward=season_length)
+    print(y_pred)
+
+
+def train(data: pd.DataFrame) -> dict:
+    """
+    Train a model on the given data.
+
+    @param data: The data to train on.
+    Leftmost column is the time column and the rightmost column is the value column.
+    Any columns in between are exogenous variables.
+    @return: A dictionary containing the trained model and any other information.
+    """
     # Prepare dataset
-    dataset = prepare_dataset(dataset, time_col=TIME_COL, value_col=VALUE_COL)
-    print(dataset)
+    columns = data.columns
+    data = prepare_dataset(data, time_col=columns[0], value_col=columns[-1])
+    print(data)
 
     # Create model
-    season_length = get_seasonal_period(dataset)
+    season_length = get_seasonal_period(data)
     statsmodel = AutoTheta(season_length=season_length)
     model = StatsforecastWrapper(model=statsmodel, scorer=calculate_smape)
 
     # Prepare train and test data
-    X = dataset.value
-    X_train, X_test = train_test_split(X, test_size=0.1, shuffle=False)
+    y = data.value
+    y_train, y_test = train_test_split(y, test_size=0.1, shuffle=False)
 
     # Fit model on train data
-    model.fit(X_train)
+    model.fit(y_train)
 
     # Plot test data and print test score
-    test_score = model.score(X_train, X_test)
+    test_score = model.score(y_train, y_test)
     print(f"Test score: {test_score:.2f}")
-    model.plot_prediction(X_test, X_test)
+    model.plot_prediction(y_test, y_test)  # TODO: Move model cross validation to abstract class
 
     # Fit model on all data
-    model.fit(X)
+    model.fit(y)
 
-    # Predict next steps
-    y_pred = model.predict(X, lookforward=season_length)
-    print(y_pred)
+    # Return model and any other information
+    return {'model': model,
+            'evaluation': {'sMAPE': test_score},
+            'stats': {'season_length': season_length}}
+
+
+def predict(model: AbstractModel, lookforward: int) -> np.array:
+    """
+    Predict the next lookforward steps.
+
+    :param model: Trained model as returned by train().
+    :param lookforward: Number of forward steps to predict.
+    :return: Array of predicted values of shape (lookforward,).
+    """
+    return model.predict(lookforward=lookforward)
 
 
 # TODO: Integrate multivariate datasets
 # TODO: How to add features?
 def prepare_dataset(dataset: pd.DataFrame, time_col: str, value_col: str) -> pd.DataFrame:
+    """
+    Prepare the dataset for training or prediction.
+
+    :param dataset: Dataframe containing the dataset.
+    :param time_col: Column name identifying the time component.
+    :param value_col: Column name identifying the value component.
+    :return: Dataframe with the time and value columns renamed to 'datetime' and 'value' respectively.
+    """
     clean_dataset = dataset.rename(columns={time_col: 'datetime',
                                             value_col: 'value'})
     clean_dataset['datetime'] = pd.to_datetime(clean_dataset['datetime'])
--- modelwrappers.py
+++ modelwrappers.py
@@ -12,23 +12,32 @@ from abc import ABC, abstractmethod
 class AbstractModel(ABC):
     """
     Abstract class used for both Base ("Sibyl") and Meta ("Pythia") models.
+    Modeled after the Nixtla library's model interface.
     """
     def __init__(self, scorer: Callable[[np.ndarray|pd.DataFrame, np.ndarray|pd.Series], float]):
         self.scorer = scorer
 
-    def score(self, X: np.ndarray|pd.DataFrame, y: np.ndarray|pd.Series) -> float:
+    def score(self, y: np.ndarray|pd.Series, X: np.ndarray|pd.DataFrame=None) -> float:
         """
         Score the model on the given data.
-        """
-        return self.scorer(y, self.predict(X, len(y)))
 
-    def plot_prediction(self, X:np.ndarray|pd.DataFrame, y:np.ndarray|pd.Series):
+        @param y: Future values of time series of shape (t,).
+        @param X: Exogenous variables of shape (t, n).
+        @return: The score of the model on the given data.
         """
-        Plot the actual and predicted values.
+        return self.scorer(y, self.predict(len(y), X))
+
+    def plot_prediction(self, y:np.ndarray|pd.Series, X:np.ndarray|pd.DataFrame=None):
+        """
+        Plot the actual and predicted values on a line chart.
+
+        @param y: Future values of time series of shape (t,).
+        @param X: Exogenous variables of shape (t, n).
+        @return: The score of the model on the given data.
         """
         index = range(len(y)) if isinstance(y, np.ndarray) else y.index
         plt.plot(index, y, label='Actual')
-        y_pred = self.predict(X, lookforward=len(y))
+        y_pred = self.predict(lookforward=len(y), X=X)
         plt.plot(index, y_pred, label='Model')
         plt.xlabel('Time')
         plt.xticks(rotation=45)
@@ -37,16 +46,24 @@ class AbstractModel(ABC):
         plt.show()
 
     @abstractmethod
-    def fit(self, X: np.ndarray|pd.DataFrame, y: np.ndarray|pd.Series=None) -> float:
+    def fit(self, y: np.ndarray|pd.Series, X: np.ndarray|pd.DataFrame=None) -> float:
         """
         Fit the selected model.
+
+        @param y: Time series of shape (t,).
+        @param X: Exogenous variables of shape (t, n).
+        @return: The score on the training data.
         """
         pass
 
     @abstractmethod
-    def predict(self, X: np.ndarray|pd.DataFrame, lookforward: int=1) -> np.ndarray:
+    def predict(self, lookforward: int=1, X: np.ndarray|pd.DataFrame=None) -> np.ndarray:
         """
         Predict the next lookforward steps.
+
+        @param lookforward: Number of steps to predict.
+        @param X: Exogenous variables of shape (lookforward, n_x).
+        @return: An array with the predicted values of shape (lookforward,).
         """
         pass
 
@@ -56,26 +73,24 @@ class StatsforecastWrapper(AbstractModel):
     Wrapper for statsforecast models according to the AbstractModel interface.
     """
     def __init__(self, model, *args, **kwargs):
-        """
-        Initialize the wrapper with a specific model type.
-        """
         self.model = model
         super().__init__(*args, **kwargs)
 
-    def fit(self, X: np.ndarray|pd.DataFrame, y: np.ndarray|pd.Series=None) -> float:
-        """
-        Fit the selected model.
-        """
-        self.model.fit(self._validate_input_array(X))
+    def fit(self, y: np.ndarray|pd.Series, X: np.ndarray|pd.DataFrame=None) -> float:
+        y_val = self._validate_input_array(y)
+        X_val = None if X is None else self._validate_input_array(X)
+        self.model.fit(y_val, X_val)
+        return self.score(y_val, X_val)
 
-    def predict(self, X: np.ndarray|pd.DataFrame, lookforward: int=1) -> np.ndarray:
-        """
-        Predict the next lookforward steps.
-        """
-        return self.model.predict(h=lookforward)['mean']
+    def predict(self, lookforward: int=1, X: np.ndarray|pd.DataFrame=None) -> np.ndarray:
+        X_val = None if X is None else self._validate_input_array(X)
+        return self.model.predict(h=lookforward, X=X_val)['mean']
 
     def _validate_input_array(self, X: np.ndarray|pd.DataFrame) -> np.ndarray:
         """
-        Ensure the input data is a numpy array.
+        Ensure the input data is a numpy array to be used by statsforecast models.
+
+        @param X: Input data of shape (t, n).
+        @return: The input data as a numpy array.
         """
         return X if isinstance(X, np.ndarray) else X.to_numpy()
