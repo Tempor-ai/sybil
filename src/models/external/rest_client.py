23ad6c8e8d560dfe805116cff5776e4c2da597e6
Zhiqian Wang <wangzhiq@gwu.edu>
2024-03-23 13:33:28 -0400

    add logic for onboard np model to fix the issue when onboard np model return less rows than input

--- src/models/external/rest_client.py
+++ src/models/external/rest_client.py
@@ -2,6 +2,7 @@ import json
 import yaml
 import requests
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from fastapi import APIRouter
@@ -75,6 +76,25 @@ class rest_client:
         
         response = requests.post(url, json=api_json)
         new = response.json()
-        return response.json().get('forecast')  
+        new.get('forecast')
 
+        #check of response is the same length as the input data
+        forecast_yhat = response.json().get('forecast')['yhat1']
+        forecast_ds = response.json().get('forecast')['ds']
+        forecast_yhat_np=np.asarray(list(forecast_yhat.values()))
+        forecast_ds_np=np.asarray(list(forecast_ds.values()))
+        forecast_df = pd.DataFrame(data = forecast_yhat_np, index=forecast_ds_np, columns = ['prediction']) 
+        forecast_df_index= pd.to_datetime(forecast_df.index, infer_datetime_format=True).strftime('%Y-%m-%d')
+        forecast_df.set_index(forecast_df_index, inplace=True)
+        forecast_response_json = response.json().get('forecast')
+        if (forecast_df.shape[0] != dataset.shape[0]):
+            # pad the forecast result if forecast_yhat_df or forecast_ds_df has less rows than the input dataset
+            merged_df = dataset.join(forecast_df)['prediction'].fillna(forecast_yhat_np[0])
+            merged_df.index = merged_df.index.set_names([0])
+            merged_df = merged_df.reset_index(0)
+            merged_df[0] = pd.to_datetime(merged_df[0], infer_datetime_format=True)
+            merged_df[0] = merged_df[0].apply(lambda x:x.isoformat())
+            forecast_response_json['yhat1'] = merged_df['prediction'].to_dict()
+            forecast_response_json['ds'] = merged_df[0].to_dict()
 
+        return forecast_response_json 
