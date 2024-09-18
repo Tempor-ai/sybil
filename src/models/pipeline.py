427c91531e3b577337bef2fb88b41343c8a6e932
sairam-gwu <sairam.venkatachalam@gwu.edu>
2024-09-18 18:43:45 -0400

    Updated pipeline for preprocessors

--- src/models/pipeline.py
+++ src/models/pipeline.py
@@ -5,7 +5,7 @@ Pipeline of transformers and models.
 import numpy as np
 import pandas as pd
 from .modelwrappers import AbstractModel
-from .preprocessor import AbstractPreprocessor
+from .preprocessor import AbstractPreprocessor,MinMaxScaler
 from typing import List
 
 
@@ -23,13 +23,19 @@ class Pipeline(AbstractModel):
         for transformer in self.transformers:
             if X is not None:
                 X = transformer.fit_transform(X)
+            if isinstance(transformer, MinMaxScaler):
+               y=transformer.fit_transform(y)
         return self.model._train(y, X)
     
     def _predict(self, lookforward: int=1, X: pd.DataFrame=None)-> np.ndarray:
+        
         if X is not None:
             for transformer in self.transformers:
                 X = transformer.transform(X)
         y = self.model.predict(lookforward, X)
+        for transformer in self.transformers:
+            if isinstance(transformer, MinMaxScaler):
+                y = transformer.inverse_transform(y)
         return y
     
 class ExternalPipeline(AbstractModel):
