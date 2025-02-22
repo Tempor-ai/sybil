a77ddee4b52eff6f6dd1ad4f6ad31fc506b461b2
sairam-gwu <sairam.venkatachalam@gwu.edu>
2025-02-22 13:00:36 -0500

    Added routing conditions for mauq and user paramters

--- src/appserver/routes.py
+++ src/appserver/routes.py
@@ -10,7 +10,7 @@ from fastapi import APIRouter
 from fastapi.encoders import jsonable_encoder
 import yaml
 import requests
-
+from typing import Optional
 from models.modelfactory import ModelFactory
 
 
@@ -33,6 +33,7 @@ class Model(BaseModel):
     # params: Union[Parameters, None] = None
     external_params: Union[dict, None] = None
     params: dict
+    mauq_params: Optional[dict] = None 
 
 class Metric(BaseModel):
     type: str
@@ -46,12 +47,12 @@ class TrainResponse(BaseModel):
     model: str
     type: str
     metrics: Union[List[Metric], None] = None
-    estimate: float
+    estimate: Optional[float] = None
 
 class ForecastRequest(BaseModel):
     model: str
     data: Union[List[DATASET_VALUE], List[List[DATASET_VALUE]]]
-    estimate: float
+    estimate: Optional[float] = None
 
 class ForecastResponse(BaseModel):
     data: List[List[DATASET_VALUE]]
@@ -70,7 +71,12 @@ async def train(train_request: TrainRequest):
     dataset = ModelFactory.prepare_dataset(pd.DataFrame(train_request.data))
 
     # Get optional user specs
-    model_info = train_request.model
+    
+    #Check if MAUQ params are passed
+    # model_info = {k: v for k, v in train_request.model.params.items() if k != 'mauq_params'} if train_request.model and train_request.model.params else {}
+
+    model_info = {k: v for k, v in train_request.model.dict().items() if k != 'mauq_params'} if train_request.model else {}
+
 
     # If user did not pass in the model spec
     if model_info is None:
@@ -84,8 +90,6 @@ async def train(train_request: TrainRequest):
     # Train model
     training_info = model.train(dataset)
 
-    # Serialize, compress, and finally encode model in base64 ASCII, so it can be sent in JSON
-    # output_model = base64.b64encode(blosc.compress(pickle.dumps(training_info['model'])))
 
     output_model = ModelFactory.save(training_info['model'])
 
@@ -99,38 +103,43 @@ async def train(train_request: TrainRequest):
     if len(metrics) == 0:
         metrics = None
 
-## MAUQ API CALL
-    yact=training_info['yact']
-    ypred=training_info['ypred']
-    mauq_df =pd.DataFrame({
-    'y_act': yact.values,
-    'y_pred': ypred.values
-    })
+    if train_request.model.mauq_params is not None:
 
-    api_json = {
-        'data': mauq_df.values.tolist(),
-        'test':mauq_df.values.tolist(),
-        'problem_type': 'regression',
-        'confidence_level': 0.7,
-        'output_type': 'estimate'
-    }
+        ## MAUQ API CALL
+        yact=training_info['yact']
+        ypred=training_info['ypred']
+        mauq_df =pd.DataFrame({
+        'y_act': yact.values,
+        'y_pred': ypred.values
+        })
 
-    # with open('mauq_url.yaml', 'r') as file:
-    #     url_dict = yaml.safe_load(file)
-    # URL to our MAUQ AWS service
-    protocol = 'http'
-    host = 'localhost'
-    port = 8001
-    endpoint = 'quantify-uncertainty'
+        confidence_level=train_request.model.mauq_params['confidence_level']
+        api_json = {
+            'data': mauq_df.values.tolist(),
+            'test':mauq_df.values.tolist(),
+            'problem_type': 'regression',
+            'confidence_level': confidence_level,
+            'output_type': 'estimate'
+        }
 
-    url = '%s://%s:%s/%s' % (protocol, host, str(port), endpoint)
-    response = requests.post(url, json=api_json)
-    
-    # There is dynamacism in the metrics field
-    return TrainResponse(model=output_model,
+        protocol = 'http'
+        host = 'localhost'
+        port = 8001
+        endpoint = 'quantify-uncertainty'
+
+        url = '%s://%s:%s/%s' % (protocol, host, str(port), endpoint)
+        response = requests.post(url, json=api_json)
+        
+        # There is dynamacism in the metrics field
+        return TrainResponse(model=output_model,
+                            type=training_info["type"],
+                            metrics=metrics,
+                            estimate=response.json()['output']
+                            )
+    else:
+        return TrainResponse(model=output_model,
                          type=training_info["type"],
-                         metrics=metrics,
-                         estimate=response.json()['output']
+                         metrics=metrics
                          )
 
 @router.post('/forecast')
@@ -155,21 +164,24 @@ async def forecast(forecast_request: ForecastRequest):
     output = model.predict(lookforward=num_steps, X=dataset).reset_index()
     output['index'] = dates
 
-    estimate=forecast_request.estimate
-    # Calling MAUQ Estimate to columns
-    api_json = {
-        'test': output.values.tolist(),
-        'estimate': estimate
-    }
+    if forecast_request.estimate is not None:
+        estimate=forecast_request.estimate
+        # Calling MAUQ Estimate to columns
+        api_json = {
+            'test': output.values.tolist(),
+            'estimate': estimate
+        }
 
-    protocol = 'http'
-    host = 'localhost'
-    port = 8001
-    endpoint = 'estimate to columns'
+        protocol = 'http'
+        host = 'localhost'
+        port = 8001
+        endpoint = 'estimate-to-columns'
 
-    url = '%s://%s:%s/%s' % (protocol, host, str(port), endpoint)
-    response = requests.post(url, json=api_json)
-    output=pd.DataFrame(response.json()['output'])
+        url = '%s://%s:%s/%s' % (protocol, host, str(port), endpoint)
+        response = requests.post(url, json=api_json)
+        output=pd.DataFrame(response.json()['output'])
+
+
     output = output.values.tolist()
     
 
